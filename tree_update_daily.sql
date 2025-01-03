2d69f394ab21db6df4f6448d6bc4d361564db79c
Mohammad Shahidullah Chowdhury <124570027+shahidullahmc@users.noreply.github.com>
2025-01-03 09:31:06 +0600

    Add files via upload

---
+++ tree_update_daily.sql
@@ -0,0 +1,20 @@
+SELECT
+ TIMESTAMP_TRUNC(plant_date, MONTH) as plant_month,
+  COUNT(tree_id) AS total_trees,
+  species,
+  care_taker,
+  address,
+  site_info
+FROM
+  `bigquery-public-data.san_francisco_trees.street_trees`
+
+WHERE
+  address IS NOT NULL
+  AND plant_date >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)
+  AND plant_date < TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY)
+GROUP BY
+  plant_month,
+  species,
+  care_taker,
+  address,
+  site_info
